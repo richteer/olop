fbd7b25432889e2265ecd18c11314a162db87d21
Eric Richter <richteer@lastprime.net>
2014-12-11 15:25:58 -0500

    Now takes input on stdin, prints to stdout

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,1 @@
-(lambda ls, qs: qs(ls, qs))([7,2,6,4,5,1,2], lambda ls, qs: ls if len(ls) == 1 or len(ls) == 0 else qs([i for i in ls[1:] if i < ls[0]], qs) + [ls[0]] + qs([i for i in ls[1:] if i >= ls[0]], qs))
+print((lambda ls, qs: qs(ls, qs))(eval(input()), lambda ls, qs: ls if len(ls) == 1 or len(ls) == 0 else qs([i for i in ls[1:] if i < ls[0]], qs) + [ls[0]] + qs([i for i in ls[1:] if i >= ls[0]], qs)))
